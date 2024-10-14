9d395297ffccbcebd1c637139a7e47efff391351
Gabriel <gabriel.rskm@gmail.com>
2024-10-14 18:03:32 -0300

    primera entrega de informe, incompleta pero para tener una devolucion

---
+++ include/devolucion-cliente.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "fecha.h"
+#include "productos.h"
+
+class DevolucionCliente{
+  private:
+   int cantidad;
+   Producto producto;
+   Fecha fecha;
+   char motivo[50];
+};
---
+++ include/productos.h
@@ -0,0 +1,6 @@
+#pragma once
+
+class Productos{
+   int _costoTotal;
+   int cantidad;
+};
